c8d2c345f6f1b44af2326d628eba7bbcf4b45cd3
aaizaguirre <aaizaguirreo@gmail.com>
2023-06-12 09:24:23 -0500

    Ejemplos ciclos while y for

---
+++ 1-Java_JRE_JDK/EjemploCiclos.java
@@ -0,0 +1,10 @@
+public class EjemploCiclos {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while (contador < 10){
+            System.out.print(contador);
+            contador += 1;
+        }
+    }
+}
---
+++ 1-Java_JRE_JDK/EjercicioCiclo.java
@@ -0,0 +1,14 @@
+public class EjercicioCiclo {
+
+    public static void main(String args[]) {
+
+        for(int fila = 1; fila < 6; fila++) {
+            for (int columna = 1; columna <= fila; columna++) {
+                System.out.print(columna);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
