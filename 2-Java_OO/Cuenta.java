56799309b6b4efc452cb8b0c41d4db01330e2757
aaizaguirre <aaizaguirreo@gmail.com>
2023-06-14 04:43:09 -0500

    Método retirar y transferir, uso del 'this'

--- 2-Java_OO/Cuenta.java
+++ 2-Java_OO/Cuenta.java
@@ -10,7 +10,27 @@ class Cuenta{
     int numero;
     String titular;
 
-    void depositar(double cantidad){  // Método para depositar dinero a cuenta, donde cantidad es la cantidad de dinero. Void hace referencia a un metodo que no retorna ningun valor.
-        saldo += cantidad;
+    public void depositar(double cantidad){  // Método para depositar dinero a cuenta, donde cantidad es la cantidad de dinero. Void hace referencia a un metodo que no retorna ningun valor.
+        // saldo += cantidad;
+        this.saldo = this.saldo + cantidad;  // This hace referencia al saldo de la cuenta con la que se llama el metodo.
     } 
+
+    public boolean retirar(double cantidad){  // Devuelve un booleano.
+        if(this.saldo >= cantidad){
+            this.saldo = this.saldo - cantidad;
+            return true;                      // Si la condición es verdadera se ejecuta este fragmento de codigo.
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transferir(double cantidad, Cuenta cuenta){
+        if(this.saldo >= cantidad){
+            this.saldo = this.saldo - cantidad;
+            cuenta.depositar(cantidad);
+            return true;                   
+        } 
+        return false;
+    }
+
 }
--- 2-Java_OO/PruebaMetodos.java
+++ 2-Java_OO/PruebaMetodos.java
@@ -5,5 +5,16 @@ public class PruebaMetodos {
 
         miCuenta.depositar(200);
         System.out.println(miCuenta.saldo);
+
+        miCuenta.retirar(2000);
+        System.out.println(miCuenta.saldo);
+
+        Cuenta cuentaJimena = new Cuenta();
+
+        cuentaJimena.depositar(1000);
+        cuentaJimena.transferir(400, miCuenta);
+        
+        System.out.println(miCuenta.saldo);
+        System.out.println(cuentaJimena.saldo);
     }   
 }
